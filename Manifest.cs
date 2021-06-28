335e04e143f8646367cc880452e60742dc7c7cca
Joe Berkley <joe.berkley@etchuk.com>
2021-06-28 12:11:13 +0100

    Add filter to handle ordinal dates, as well as inital project structure

---
+++ Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Author = "Etch UK Ltd.",
+    Category = "Content",
+    Description = "A collection of helpful Liquid filters",
+    Name = "Etch Liquid Filters",
+    Version = "0.0.1",
+    Website = "https://etchuk.com"
+)]
