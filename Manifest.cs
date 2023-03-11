1d54820fd05dc1cc35752fd05274201be4253fb2
Peter Keating <pete.keating@etchuk.com>
2023-03-11 09:39:52 +0000

    Make `Environment` object available to Liquid templates
    `Environment` object will have properties for whether environment is
    development, staging or production.

--- Manifest.cs
+++ Manifest.cs
@@ -3,7 +3,7 @@ using OrchardCore.Modules.Manifest;
 [assembly: Module(
     Author = "Etch UK Ltd.",
     Category = "Content",
-    Description = "A collection of helpful Liquid filters",
+    Description = "Collection of helpful Liquid filters.",
     Name = "Etch Liquid Filters",
     Version = "$(VersionNumber)",
     Website = "https://etchuk.com"
--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,8 @@
 ﻿using Etch.OrchardCore.Liquid.Filters;
+using Fluid;
+using Fluid.Values;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using OrchardCore.Liquid;
 using OrchardCore.Modules;
 
@@ -10,6 +13,28 @@ namespace Etch.OrchardCore.Liquid
         public override void ConfigureServices(IServiceCollection services)
         {
             services.AddLiquidFilter<OrdinalDateFilter>("ordinal_date");
+
+            services.Configure<TemplateOptions>(o =>
+            {
+                o.Scope.SetValue("Environment", new ObjectValue(new LiquidEnvironmentAccessor()));
+                o.MemberAccessStrategy.Register<LiquidEnvironmentAccessor, FluidValue>((obj, name, context) =>
+                {
+                    var liquidTemplateContext = (LiquidTemplateContext)context;
+                    var hostEnvironment = liquidTemplateContext.Services.GetRequiredService<IHostEnvironment>();
+
+                    FluidValue result = name switch
+                    {
+                        nameof(hostEnvironment.ApplicationName) => new StringValue(hostEnvironment.ApplicationName),
+                        nameof(hostEnvironment.EnvironmentName) => new StringValue(hostEnvironment.EnvironmentName),
+                        "IsDevelopment" => BooleanValue.Create(hostEnvironment.IsDevelopment()),
+                        "IsStaging" => BooleanValue.Create(hostEnvironment.IsStaging()),
+                        "IsProduction" => BooleanValue.Create(hostEnvironment.IsProduction()),
+                        _ => NilValue.Instance
+                    };
+
+                    return result;
+                });
+            });
         }
     }
 }
